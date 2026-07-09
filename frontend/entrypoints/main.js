32d3c767c67f1e74c4283417239f3ae78a612f0f
renebreslauer <renebreslauer@me.com>
2026-07-09 10:35:33 -0400

    feat(homepage qa): image optimization for various breakpoints

--- frontend/entrypoints/main.js
+++ frontend/entrypoints/main.js
@@ -16,6 +16,7 @@ import '../styles/header.css';
 import '../styles/media-gallery-bundle.css';
 import '../styles/main-product.css';
 import '../styles/products-slider.css';
+import '../styles/multi-coll-carousel.css';
 
 
 
